2f6cd0abe63762e15654b51855535d0c8cb4460a
Hugo Vega <hugotcc@gmail.com>
2016-08-26 12:43:26 +0200

    Part 1 done

---
+++ ch04/todo_crud.ex
@@ -0,0 +1,34 @@
+defmodule TodoList do
+
+  defstruct [auto_id: 1, entries: Map.new]
+
+  def new, do: %TodoList{}
+
+  def add_entry(%TodoList{auto_id: auto_id, entries: entries} = todo_list, entry) do
+   entry = Map.put(entry, :id, auto_id)
+   new_entries = Map.put(entries, auto_id, entry)
+   %TodoList{todo_list | entries: new_entries, auto_id: auto_id + 1}
+  end
+
+  def entries(%TodoList{entries: entries}, date) do
+    entries
+      |> Stream.filter(fn({_, entry}) -> entry.date == date end)
+      |> Enum.map(fn({_, entry}) -> entry end)
+  end
+
+  def update_entry(%TodoList{entries: entries} = todo_list, entry_id, updater_fun) do
+    case entries[entry_id] do
+      nil -> todo_list
+      old_entry ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(%TodoList{entries: entries} = todo_list, entry_id) do
+    %TodoList{todo_list | entries: Map.delete(entries, entry_id)}
+  end
+
+
+end
